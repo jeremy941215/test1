f8883af4773048eed102b55d0402e51d50452fef
jeremy941215 <43679929+jeremy941215@users.noreply.github.com>
2023-06-05 15:08:39 +0800

    Add files via upload

---
+++ 111/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/01100/pen/OJoQZpV](https://codepen.io/01100/pen/OJoQZpV).
+
---
+++ 111/src/script.js
@@ -0,0 +1,16 @@
+
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
+
+
+
+
+
+
+
